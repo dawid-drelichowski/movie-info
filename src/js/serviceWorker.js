11666687489b20d89c25b9241c2ed9fe3369d534
Dawid Drelichowski <dawid.drelichowski@gmail.com>
2018-12-20 20:02:10 +0100

    Service worker improvements

--- src/js/serviceWorker.js
+++ src/js/serviceWorker.js
@@ -1,9 +1,10 @@
 /*
   @todo:
-    - movies and movie images cache separation
+    - movies and movie images cache refactoring (find common parts)
     - cache validation, cache cleaing, available disk space checking
     - worker update testing
     - usage of babel, webpack configuration, migration to async/await
+    - tests
  */
 import config from 'config.json'
 
@@ -20,7 +21,8 @@ self.addEventListener('activate', event => {
       return Promise.all(keys.map(key => {
         if (![
           config.serviceWorker.caches.assets.name,
-          config.serviceWorker.caches.movies.name
+          config.serviceWorker.caches.movies.name,
+          config.serviceWorker.caches.images.name
         ].includes(key)) {
           return caches.delete(key)
         }
@@ -31,24 +33,40 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-  if (event.request.url.search(`^${config.movieDb.url}|${config.movieDb.images.url}`) > -1) {
-    event.respondWith(
-      caches.open(config.serviceWorker.caches.movies.name).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            cache.put(event.request.url, response.clone())
-            return response
-          })
-          .catch(error => {
-            return caches.match(event.request) || error
-          })
-      })
-    )
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request)
-      })
-    )
+  switch (true) {
+    case event.request.url.indexOf(config.movieDb.url) > -1:
+      event.respondWith(
+        caches.open(config.serviceWorker.caches.movies.name).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request.url, response.clone())
+              return response
+            })
+            .catch(error => {
+              return caches.match(event.request) || error
+            })
+        })
+      )
+      break
+    case event.request.url.indexOf(config.movieDb.images.url) > -1:
+      event.respondWith(
+        caches.open(config.serviceWorker.caches.images.name).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request.url, response.clone())
+              return response
+            })
+            .catch(error => {
+              return caches.match(event.request) || error
+            })
+        })
+      )
+      break
+    default:
+      event.respondWith(
+        caches.match(event.request).then(response => {
+          return response || fetch(event.request)
+        })
+      )
   }
 })
